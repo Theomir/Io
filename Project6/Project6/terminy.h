36f3af4d1bcd7a1d3d9a5901d28ec50143428b42
Grzegorz Adamczyk <grzegorz.adamczyk1993@gmail.com>
2016-01-21 12:23:21 +0100

    terminy.h komentarze

--- Project6/Project6/terminy.h
+++ Project6/Project6/terminy.h
@@ -3,28 +3,28 @@
 #include <iostream>
 using namespace std;
 const string miesiace[] =
-{ "", "styczen", "luty", "marzec", "kwiecien", "maj", "czerwiec", "lipiec", "sierpien", "wrzesien", "pazdziernik", "listopad", "grudzien" };
-const int dlugosc_miesiecy[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-const int dlugosc_miesiecy_przyrostowo[] = { 0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+{ "", "styczen", "luty", "marzec", "kwiecien", "maj", "czerwiec", "lipiec", "sierpien", "wrzesien", "pazdziernik", "listopad", "grudzien" };//tablica miesiecy
+const int dlugosc_miesiecy[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };//tablica dl miesiecy
+const int dlugosc_miesiecy_przyrostowo[] = { 0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };//tablica dlugosci przytostowej miesiecy
 class terminy
 {
-	int dzien;
-	int miesiac;
-	int rok;
+	int dzien; //numer dnia
+	int miesiac;//numer miesiaca
+	int rok;//nuker roku
 public:
 	terminy()
-	{ }
+	{ }//konstruktor
 	terminy(int g, int m, int a) : dzien(g), miesiac(m), rok(a)
-	{ }
-	void input(string tekst = "| Wstaw termin");
-	bool input_from_string(string str_data);
-	string to_string() const;
-	string to_csv() const;
-	bool operator == (const terminy & d) const;
-	bool operator < (const terminy & d) const;
-	terminy operator ++ (int);
-	bool format_daty();
-	void dataBiezaca();
-	friend ostream& operator<<(ostream&, terminy&);
+	{ }//konstruktor 2
+	void input(string tekst = "| Wstaw termin");//wprowadzanie danych
+	bool input_from_string(string str_data);//wprowadzanie ze stringa
+	string to_string() const;//konwertorowanie do stringa
+	string to_csv() const;//konwerterowanie do csv
+	bool operator == (const terminy & d) const;//przeciazenie operatora ==
+	bool operator < (const terminy & d) const;//przeciazenie operatora <
+	terminy operator ++ (int); // przeciazenie operatora ++
+	bool format_daty(); //format daty
+	void dataBiezaca();//data aktualna z systemu
+	friend ostream& operator<<(ostream&, terminy&); //przeciazenie operatora <<
 };
 #endif
