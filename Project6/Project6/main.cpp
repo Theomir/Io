8b5227a633206d617221adba6957584e03b55ebf
Grzegorz Adamczyk <grzegorz.adamczyk1993@gmail.com>
2016-01-21 20:50:35 +0100

    komentarz w mainie

--- Project6/Project6/main.cpp
+++ Project6/Project6/main.cpp
@@ -23,27 +23,26 @@ int main()
 	{
 		pokaz_menu(a_glowna.wypiszTerminy(data1), a_glowna.wypiszTerminy(data2));
 		n_op = int_input(" Co zrobic: ", 1, 14);
-
 		switch (n_op)
 		{
-		case 1:
+		case 1:  //dowdawanie wydarzenia
 			a_glowna.dodaj_wydarzenie();
 			pauza();
 			break;
 		case 2:
-			data1.dataBiezaca();
+			data1.dataBiezaca(); // wsywietl wydarzenia na dzis
 			a_glowna.wyswietl_wydarzenia_dnia(data1);
 			break;
 		case 3:
-			data1.dataBiezaca();
+			data1.dataBiezaca(); //wysietl wydarzenia na jutro
 			data1++;
 			a_glowna.wyswietl_wydarzenia_dnia(data1);
 			break;
-		case 4:
+		case 4: //wyswietl wydarzenia dla konkretnego dnia
 			data1.input();
 			a_glowna.wyswietl_wydarzenia_dnia(data1);
 			break;
-		case 5:
+		case 5://wyswietl wydarzenia w najblizszych dniach
 			temp1 = int_input("Liczba dni (1-30): ", 1, 30);
 			data1.dataBiezaca();
 			wypisz_naglowek("WYDARZENIA NA NASTEPNE " + int_to_string(temp1) + " DNI");
@@ -51,7 +50,7 @@ int main()
 				a_glowna.wyswietl_wydarzenia_dnia(data1++, false);
 			pauza();
 			break;
-		case 6:
+		case 6: // wyswietl wydarzenia od do
 			data1.input(" od ");
 			data2.input(" Do ");
 			wypisz_naglowek("WYDARZENIA OD " + data1.to_string() + " DO " + data2++.to_string());
@@ -59,37 +58,37 @@ int main()
 				a_glowna.wyswietl_wydarzenia_dnia(data1++, false);
 			pauza();
 			break;
-		case 7:
+		case 7://wypisz terminy w najblizszych tygodniach
 			a_glowna.wypiszTerminy();
 			break;
-		case 8:
+		case 8://wyswietl ksiazke adrsowa
 			r_glowna.wypisz_adres();
 			break;
-		case 9:
+		case 9://dodaj nowy kontakt
 			r_glowna.dodaj_kontakt();
 			break;
-		case 10:
+		case 10://usun kontakt
 			r_glowna.usun_kontakt();
 			break;
-		case 11:
+		case 11://ususn wydarzenie
 			a_glowna.usun_wydarzenie();
 			break;
-		case 12:
+		case 12://usun wygasniete wydarzenia
 			if (potwierdzenie(" Usunac wygasniete wydarzenia (y/n) ?"))
 				a_glowna.usun_wygasniete();
 			break;
-		case 14:
+		case 14://wychodzenie bez zapisywania
 			if (potwierdzenie(" Czy chcesz wyjsc bez zapisywania(y/n) ?"))
 				exit(1);
 			break;
 		}
-	} while (n_op != 13);
+	} while (n_op != 13);// zapisz i wyjdz
 	while (!r_glowna.zapiszPlik(ksiazka_plik) || !a_glowna.zapiszPlik(kalendarz_plik))
 	{
 		cout << "\t[blad zapisywania pliku]\n";
 		pauza();
 	}
-	cout << "\t[ Zmiany prawidlowo zapisane w pliku ]\n";
+	cout << "\t[ Zmiany prawidlowo zapisane w pliku ]\n";//zapisanie poprawne plików
 	pauza();
 	return 0;
 }
